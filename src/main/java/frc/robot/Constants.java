b70e1fc0b6e828a990af965ad16893ee09059c6e
blade9540 <149724929+blade9540@users.noreply.github.com>
2026-03-02 17:55:49 -0500

    constantsand swerve widget

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,6 +22,11 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  //serve constants
+  
+
+  //id constants
   public static final int Hood_ID = 48;
   public static final int Flywheel_Lead_ID = 46;
   public static final int Flywheel_Follower_ID = 47;
@@ -32,27 +37,27 @@ public final class Constants {
 
 
   public static final int kDriverControllerPort = 0;
-    public static final int LEFT_LEADER_ID = 1;
-    public static double turnVoltage = 1;
-    public static boolean stopbutton = false;
-    public static double yaw;
-    public static double targetYaw = 0.0;
-    public static double currentYaw;
-    public static double currentSkew;
-    public static boolean targetVisible = false;
-    public static int framesMissed = 0; //How many frames has the turret not seen the april tag
-    public static final double CameraHeight = .635;
-    public static final double TagHeight = 1.084;
-    public static final double CameraPitch = 0;
-    public static final double TagPitch = 0;
+  public static final int LEFT_LEADER_ID = 1;
+  public static double turnVoltage = 1;
+  public static boolean stopbutton = false;
+  public static double yaw;
+  public static double targetYaw = 0.0;
+  public static double currentYaw;
+  public static double currentSkew;
+  public static boolean targetVisible = false;
+  public static int framesMissed = 0; //How many frames has the turret not seen the april tag
+  public static final double CameraHeight = .635;
+  public static final double TagHeight = 1.084;
+  public static final double CameraPitch = 0;
+  public static final double TagPitch = 0;
 // middle of the hub red: 13.0155555556, 4.41236111111
 // middle of the hub blue: 5.05861111111, 4.41236111111
-    public static double RedHubX = 13.0155555556;
-    public static double RedHubY = 4.41236111111;
-    public static double BlueHubX = 5.05861111111;
-    public static double BlueHubY = 4.41236111111;
+  public static double RedHubX = 13.0155555556;
+  public static double RedHubY = 4.41236111111;
+  public static double BlueHubX = 5.05861111111;
+  public static double BlueHubY = 4.41236111111;
 
-    public static final AprilTagFieldLayout kTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
-    public static final Transform3d kRobotToCam = new Transform3d(new Translation3d(0.0, 0.0, 0.635), new Rotation3d(0, 0, 0));
+  public static final AprilTagFieldLayout kTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+  public static final Transform3d kRobotToCam = new Transform3d(new Translation3d(0.0, 0.0, 0.635), new Rotation3d(0, 0, 0));
   public static double intakeSpeed = 0.3;
 }
