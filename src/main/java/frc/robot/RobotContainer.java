43e9fe9a5226e6475498a2d1b054ad0566be9951
blade9540 <149724929+blade9540@users.noreply.github.com>
2026-03-03 15:39:59 -0500

    forgot to save snd push this stuff.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -37,7 +37,6 @@ import frc.robot.subsystems.HoodSubsystem;
 import frc.robot.subsystems.IndexAndSpindexSubsystem;
 import frc.robot.subsystems.FlywheelSubsystem;
 import frc.robot.subsystems.TurretMovement;
-//import frc.robot.subsystems.TurretVision;
 import frc.robot.Elastic.ElasticContainer;
 import static frc.robot.Constants.*;
 
@@ -98,20 +97,17 @@ public class RobotContainer {
             point.withModuleDirection(new Rotation2d(-driverController.getLeftY(), -driverController.getLeftX()))
         ));
 
-        //driverController.leftTrigger().onTrue(new TurretScan(m_turretvision, turretMovement));
-        // calls the method that turns the stopButton for Scan to true
-        //driverController.rightTrigger().onTrue(turretMovement.runOnce(()->turretMovement.getStopCommand()));
-        //driverController.povLeft().whileTrue(new TurretLeft(m_turretvision, turretMovement));
-        //driverController.povRight().whileTrue(new TurretRight(m_turretvision, turretMovement));
-        // Regular Shooting
-        //driverController.rightBumper().whileTrue(new IndexAndSpindexCommand(InSSubsystem, false, hoodSubsystem));
-
-
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
         //operatorController.b().whileTrue(new Climb(climber));
         //operatorController.x().whileTrue(new IntakeMotor(motorIntake));
         //operatorController.x().onTrue(pneumaticIntake);
+        //driverController.leftTrigger().onTrue(new TurretScan(m_turretvision, turretMovement));
+        // calls the method that turns the stopButton for Scan to true
+        //driverController.rightTrigger().onTrue(turretMovement.runOnce(()->turretMovement.getStopCommand()));
+        //driverController.povLeft().whileTrue(new TurretLeft(m_turretvision, turretMovement));
+        //driverController.povRight().whileTrue(new TurretRight(m_turretvision, turretMovement));
+
         // Spin flywheel and start hood
         //operatorController.a().onTrue(new FlywheelCommand(flywheelSubsystem, 20));
     
@@ -119,7 +115,11 @@ public class RobotContainer {
         //operatorController.povUp().onTrue(new HoodCommand(hoodSubsystem, true, 0.1));
         //operatorController.povUp().onFalse(new HoodCommand(hoodSubsystem, true, 0));
         //operatorController.povDown().onTrue(new HoodCommand(hoodSubsystem, true, -0.1));
-        //operatorController.povDown().onFalse(new HoodCommand(hoodSubsystem, true, 0))
+        //operatorController.povDown().onFalse(new HoodCommand(hoodSubsystem, true, 0));
+
+        // Regular Shooting
+        //driverController.rightBumper().whileTrue(new IndexAndSpindexCommand(InSSubsystem, false, hoodSubsystem));
+
         // Force Shoot
         //operatorController.b().whileTrue(new IndexAndSpindexCommand(InSSubsystem, true, hoodSubsystem));
 
--- src/main/java/frc/robot/commands/TurretRight.java
+++ src/main/java/frc/robot/commands/TurretRight.java
@@ -3,14 +3,14 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.TurretMovement;
-import frc.robot.subsystems.TurretVision;
+import frc.robot.subsystems.PhotonVision;
 
 public class TurretRight extends Command {
-    TurretVision m_turretVision;
+    PhotonVision m_turretVision;
     TurretMovement m_shooter;
     boolean stopSwitch;
 
-    public TurretRight(TurretVision turretVision, TurretMovement shooter){
+    public TurretRight(PhotonVision turretVision, TurretMovement shooter){
         m_turretVision = turretVision;
         addRequirements(turretVision);
 
