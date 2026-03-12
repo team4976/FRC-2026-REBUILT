244a874ed0f3b5ca36a4ddefb6c801f2bab6c1c6
Needlemouse4 <1dykstrataz@hdsb.ca>
2026-03-11 20:16:50 -0400

    Added a default value of 50

--- src/main/java/frc/robot/commands/FlywheelCommand.java
+++ src/main/java/frc/robot/commands/FlywheelCommand.java
@@ -40,9 +40,12 @@ public class FlywheelCommand extends Command{
                 - (0.4148098 * Math.pow(photonVision.getDistance() + 0.5969, 2)) 
                 //SmartDashboard.getNumber("Turret Rotate", 0));
         }  */
-          
-        autoFlywheelSpeed = 31.49597 + (10.19041 * (photonVision.getDistance() + 0.5969))  
+        if (photonVision.getDistance() != 0) {
+            autoFlywheelSpeed = 31.49597 + (10.19041 * (photonVision.getDistance() + 0.5969))  
                 - (0.4148098 * Math.pow(photonVision.getDistance() + 0.5969, 2));
+        } else {
+            autoFlywheelSpeed = 50;
+        }
 
         if (operatorController.axisGreaterThan(1, 0.1).getAsBoolean()){
             manualFlywheelSpeed = operatorController.getLeftY() * -20;
