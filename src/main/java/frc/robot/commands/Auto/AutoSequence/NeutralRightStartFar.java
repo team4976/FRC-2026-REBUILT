dd31a86181c404cd39d5bd167f90b8d34c97fd11
blade9540 <149724929+blade9540@users.noreply.github.com>
2026-03-17 11:14:29 -0400

    Moved Flywheel auto commands to auto folder

--- src/main/java/frc/robot/commands/Auto/AutoSequence/NeutralRightStartFar.java
+++ src/main/java/frc/robot/commands/Auto/AutoSequence/NeutralRightStartFar.java
@@ -13,11 +13,10 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-
-import frc.robot.commands.FlywheelStart;
-import frc.robot.commands.FlywheelStop;
 import frc.robot.commands.TurretScan;
 import frc.robot.commands.Auto.AutoIndexAndSpindexCommand;
+import frc.robot.commands.Auto.FlywheelStart;
+import frc.robot.commands.Auto.FlywheelStop;
 import frc.robot.commands.Auto.IntakeExtend;
 import frc.robot.commands.Auto.IntakeRetract;
 
