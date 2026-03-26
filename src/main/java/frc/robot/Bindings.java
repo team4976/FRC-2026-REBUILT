bd42dc5b867c4f6b9a313fe9a2ada27653c9aede
blade9540 <149724929+blade9540@users.noreply.github.com>
2026-03-25 22:20:18 -0400

    Update Bindings.java

--- src/main/java/frc/robot/Bindings.java
+++ src/main/java/frc/robot/Bindings.java
@@ -87,10 +87,21 @@ public class Bindings {
         operatorController.a().toggleOnTrue(flywheelCommand);
 
         //Operator Shoot
-        operatorController.axisGreaterThan(3, 0.1).whileTrue(indexAndSpindexCommand
-        .alongWith(Commands.repeatingSequence(intakeSubsystem.jitterIntakeUp(), 
-        (intakeSubsystem.jitterIntakeDown()))
-        )).onFalse(Commands.runOnce(()->intakeCommand.end(true)));
+        operatorController
+            .axisGreaterThan(3, 0.1)
+            .whileTrue(indexAndSpindexCommand
+                .alongWith(
+                    Commands.repeatingSequence(
+                        intakeSubsystem.jitterIntakeUp(),
+                        intakeSubsystem.jitterIntakeDown()
+                    )
+                )
+            )
+            .onFalse(
+                Commands.runOnce(
+                    ()->intakeCommand.end(true)
+                )
+            );
 
         //Turret scan
         operatorController.axisGreaterThan(2, 0.1).toggleOnTrue(turretScanYaw);
