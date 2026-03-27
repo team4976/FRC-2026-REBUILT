ee166994541b619f77a4b812e5af129fa5b5261a
blade9540 <149724929+blade9540@users.noreply.github.com>
2026-03-27 08:21:37 -0400

    fixed error.

--- src/main/java/frc/robot/Bindings.java
+++ src/main/java/frc/robot/Bindings.java
@@ -69,7 +69,7 @@ public class Bindings {
         Command repeatJidderCommand = 
             Commands.repeatingSequence(
                 Commands.deadline(Commands.waitSeconds(0.20), intakeSubsystem.intakeUpCommand()),
-                Commands.deadline(Commands.waitSeconds(0.20, intakeSubsystem.intakeDownCommand())           
+                Commands.deadline(Commands.waitSeconds(0.20), intakeSubsystem.intakeDownCommand())           
             );
 
         driverController.rightBumper().whileTrue(repeatJidderCommand).onFalse(
