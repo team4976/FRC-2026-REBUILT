9a942b133c4fef77b41da26c6363bafd477a4b48
blade9540 <149724929+blade9540@users.noreply.github.com>
2026-03-30 10:12:18 -0400

    added some comments to the vision stuff so its easier to understand.

--- src/main/java/frc/robot/subsystems/PhotonVision.java
+++ src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -19,6 +19,12 @@ public class PhotonVision extends SubsystemBase{
         return hasVaildTarget;
     };
 
+    /**
+     * A class containing mwthods for the different camera objects we create. 
+     * Automatically gets the camera results in a periodic so you just have to call the methods through the object and they will always be up to date.
+     * @param cameraName The name of the camera as a string. This is what differentiates different cameras so make sure it is correct.
+     * @param logger The telemetry object of the project. Userd for calculations based on robot speed and positioning and the such.
+     */
     public PhotonVision(String cameraName, Telemetry logger){
         vision = new VisionData(cameraName, logger);
     }
@@ -28,30 +34,63 @@ public class PhotonVision extends SubsystemBase{
         vision.update();
     }
 
+    /**
+     * A method to get what hub we are looking for based on our alliance set in driverstation. Sets the hub IDs to be used in other methods.
+     */
     public void getHubPose(){
         vision.getHubPose();
     }
 
+    /**
+     * Gets the ambiguity of the target highest in the pipeline.
+     * @return The targets ambiguity as a double.
+     */
     public double getAmbiguity(){
         return vision.getAmbiguity();
     }
 
+    /**
+     * Checks if we can see any april tags. Is true if we see tags and is false if not.
+     * @return Whether we see a tag or not as a boolean.
+     */
     public boolean targetVisible() {
         return vision.targetVisible();
     }
 
+    /**
+     * Gets the yaw of the target highest in the pipeline. Is able to get any targets yaw.
+     * <p> Yaw is the distance from the center of the camera the target is on the X axis.
+     * @return The yaw of the target as a double.
+     */
     public double getAnyYaw() {
         return vision.getAnyYaw();
     }
 
+    /**
+     * Gets the Pitch of the target highest in the pipeline. Is able to get any targets yaw.
+     * <p> Pitch is the distance from the center of the camera the target is on the Y axis.
+     * @return The yaw of the target as a double.
+     */
     public double getAnyPitch() {
         return vision.getAnyPitch();
     }
 
+    /**
+     * Not a great setup on my part but this is a method for getting a list of all april tag IDs currently seen by the camera.
+     * @return The IDs seen by the camera in a list.
+     */
     public List<Double> getIDs() {
         return vision.getIDs();
     }
 
+    /**
+     * NEEDS TO BE UPDATED. 
+     * <p> A method for getting the yaw of a desired april tag based on ID. Not well made.
+     * <p> Is passed an array of tag IDs but will only return the yaw of the first tag in the array it sees.
+     * This is only useful if we can properly triangulate the position of the hub using any of the tags we pass.
+     * @param IDs The array of IDs we want to potentially getthe yaw of
+     * @return The yaw of the first target from the list that we see.
+     */
     public OptionalDouble getTargetYaw(int[] IDs) {
         return vision.getTargetYaw(IDs);
     }
