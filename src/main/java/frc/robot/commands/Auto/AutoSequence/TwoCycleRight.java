d67ee30438d53e137db04bc8cf4343ec553e024f
blade9540 <149724929+blade9540@users.noreply.github.com>
2026-04-09 18:10:14 -0400

    added flywheelspeed in 2 cycle right auto

--- src/main/java/frc/robot/commands/Auto/AutoSequence/TwoCycleRight.java
+++ src/main/java/frc/robot/commands/Auto/AutoSequence/TwoCycleRight.java
@@ -22,6 +22,7 @@ import frc.robot.subsystems.IndexAndSpindexSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.PhotonVision;
 import frc.robot.subsystems.TurretSubsystem;
+import frc.robot.subsystems.UpdateHubInfo;
 
 public class TwoCycleRight extends SequentialCommandGroup {
 
@@ -34,6 +35,7 @@ public class TwoCycleRight extends SequentialCommandGroup {
         IntakeSubsystem intakeSubsystem = robotContainer.intakeSubsystem;
         IndexAndSpindexSubsystem indxerSubsystem = robotContainer.indexAndSpindexSubsystem;
         TurretSubsystem turretMovementSubsystem = robotContainer.turretSubsystem;
+        UpdateHubInfo updateHubInfo = robotContainer.updateHubInfo;
 
         Command OneCycleRight = AutoBuilder.buildAuto("1 Cycle - Right");
         Command OneandHalfCycleRight = AutoBuilder.buildAuto("1.5 Cycle - Right"); 
@@ -45,7 +47,7 @@ public class TwoCycleRight extends SequentialCommandGroup {
             new WaitCommand(0.5),
             OneCycleRight,
             //Commands.deadline(new WaitCommand(1), new TurretScanYaw(visionSubsystem, turretMovementSubsystem)),
-            //new FlywheelStart(flywheelSubsystem, visionSubsystem),
+            new FlywheelStart(flywheelSubsystem, visionSubsystem, updateHubInfo),
             //new AutoIndexAndSpindexCommand(indxerSubsystem, 0.8, flywheelSubsystem, intakeSubsystem),
             new WaitCommand(6),
             //Commands.deadline(Commands.waitSeconds(4), new JitterIntake(intakeSubsystem)),
--- src/main/java/frc/robot/commands/FlywheelCommand.java
+++ src/main/java/frc/robot/commands/FlywheelCommand.java
@@ -38,8 +38,6 @@ public class FlywheelCommand extends Command{
         if (updateHubInfo.getHubDistance() != 0) {
             autoFlywheelSpeed = (31.49597 + (10.19041 * (updateHubInfo.getHubDistance() + 0.5969))  
                 - (0.4148098 * Math.pow(updateHubInfo.getHubDistance()+ 0.5969, 2))) * 0.9;
-        } else {
-            autoFlywheelSpeed = 50;
         }
 
         //Sets the adder/substractor to the flywheel speed
