4e08a83c285264c1879266c9f68f82a84fb93656
Kaleful <149724997+Kaleful@users.noreply.github.com>
2026-04-10 16:45:36 -0400

    restored changes except turrenscan

--- src/main/java/frc/robot/commands/Auto/AutoSequence/OneandHalfCycleLeft.java
+++ src/main/java/frc/robot/commands/Auto/AutoSequence/OneandHalfCycleLeft.java
@@ -28,6 +28,7 @@ import frc.robot.subsystems.IndexAndSpindexSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.PhotonVision;
 import frc.robot.subsystems.TurretSubsystem;
+import frc.robot.subsystems.UpdateHubInfo;
 
 public class OneandHalfCycleLeft extends SequentialCommandGroup {
 
@@ -39,6 +40,7 @@ public class OneandHalfCycleLeft extends SequentialCommandGroup {
         IntakeSubsystem intakeSubsystem = robotContainer.intakeSubsystem;
         IndexAndSpindexSubsystem indxerSubsystem = robotContainer.indexAndSpindexSubsystem;
         TurretSubsystem turretSubsystem = robotContainer.turretSubsystem;
+        UpdateHubInfo updateHubInfo = robotContainer.updateHubInfo;
         
         Command OneCycleLeft = AutoBuilder.buildAuto("1 Cycle - Left");
         Command OneandHalfCycleLeft = AutoBuilder.buildAuto("1.5 Cycle - Left"); 
@@ -46,17 +48,17 @@ public class OneandHalfCycleLeft extends SequentialCommandGroup {
         
         addCommands(
             new PrintCommand("One and a Half Cycle Left Started"),
-            //Commands.deadline(new WaitCommand(0.2), new IntakeCommand(intakeSubsystem, false)),
+            Commands.deadline(new WaitCommand(0.2), new IntakeCommand(intakeSubsystem, false)),
             new WaitCommand(0.5),
             OneCycleLeft,
-            //Commands.deadline(new WaitCommand(1),new TurretScanYaw(visionSubsystem, turretSubsystem)),
-            //new FlywheelStart(flywheelSubsystem, visionSubsystem),
-            //new AutoIndexAndSpindexCommand(indxerSubsystem, 0.8, flywheelSubsystem, intakeSubsystem),
+            //Commands.deadline(new WaitCommand(1), new TurretScanYaw(visionSubsystem, turretSubsystem)),
+            new FlywheelStart(flywheelSubsystem, visionSubsystem, updateHubInfo),
+            new AutoIndexAndSpindexCommand(indxerSubsystem, 0.8, flywheelSubsystem, intakeSubsystem),
             new WaitCommand(6),
             //Commands.deadline(Commands.waitSeconds(6), new JitterIntake(intakeSubsystem)),
             //Commands.deadline(new WaitCommand(0.2), new IntakeCommand(intakeSubsystem, false)),
-            //new AutoIndexAndSpindexCommand(indxerSubsystem, 0.0, flywheelSubsystem, intakeSubsystem),
-            //new FlywheelStop(flywheelSubsystem, visionSubsystem),
+            new AutoIndexAndSpindexCommand(indxerSubsystem, 0.0, flywheelSubsystem, intakeSubsystem),
+            new FlywheelStop(flywheelSubsystem, visionSubsystem),
             OneandHalfCycleLeft
         );
     }
--- src/main/java/frc/robot/commands/Auto/AutoSequence/TwoCycleRight.java
+++ src/main/java/frc/robot/commands/Auto/AutoSequence/TwoCycleRight.java
@@ -43,22 +43,22 @@ public class TwoCycleRight extends SequentialCommandGroup {
         
         addCommands(
             new PrintCommand("Two Cycle Right Started"),
-            //Commands.deadline(new WaitCommand(0.2), new IntakeCommand(intakeSubsystem, false)),
+            Commands.deadline(new WaitCommand(0.2), new IntakeCommand(intakeSubsystem, false)),
             new WaitCommand(0.5),
             OneCycleRight,
             //Commands.deadline(new WaitCommand(1), new TurretScanYaw(visionSubsystem, turretMovementSubsystem)),
             new FlywheelStart(flywheelSubsystem, visionSubsystem, updateHubInfo),
-            //new AutoIndexAndSpindexCommand(indxerSubsystem, 0.8, flywheelSubsystem, intakeSubsystem),
+            new AutoIndexAndSpindexCommand(indxerSubsystem, 0.8, flywheelSubsystem, intakeSubsystem),
             new WaitCommand(6),
             //Commands.deadline(Commands.waitSeconds(4), new JitterIntake(intakeSubsystem)),
             //Commands.deadline(new WaitCommand(0.2), new IntakeCommand(intakeSubsystem, false)),
-            //new AutoIndexAndSpindexCommand(indxerSubsystem, 0.0, flywheelSubsystem, intakeSubsystem),
-            //new FlywheelStop(flywheelSubsystem, visionSubsystem),
+            new AutoIndexAndSpindexCommand(indxerSubsystem, 0.0, flywheelSubsystem, intakeSubsystem),
+            new FlywheelStop(flywheelSubsystem, visionSubsystem),
             OneandHalfCycleRight,
             TwoCycleRight,
             //Commands.deadline(new WaitCommand(1),new TurretScan(visionSubsystem, turretMovementSubsystem, true)),
-            //new FlywheelStart(flywheelSubsystem, visionSubsystem),
-            //new AutoIndexAndSpindexCommand(indxerSubsystem, 0.8, flywheelSubsystem, intakeSubsystem),
+            new FlywheelStart(flywheelSubsystem, visionSubsystem, updateHubInfo),
+            new AutoIndexAndSpindexCommand(indxerSubsystem, 0.8, flywheelSubsystem, intakeSubsystem),
             new WaitCommand(6)
         );
     }
