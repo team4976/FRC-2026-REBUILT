1e0a6b3f932e30902c7349e2207361601cb7f43b
blade9540 <149724929+blade9540@users.noreply.github.com>
2026-04-15 15:01:38 -0400

    updated vendor libraries and added tentative intake jitter to be used during auto
    jitter intake is on driver controller start for now for the sake of testing

--- src/main/java/frc/robot/commands/Auto/AutoSequence/TwoCycleRight.java
+++ src/main/java/frc/robot/commands/Auto/AutoSequence/TwoCycleRight.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.Auto.FlywheelStart;
 import frc.robot.commands.Auto.FlywheelStop;
 import frc.robot.commands.Intake.IntakeExtend;
+import frc.robot.commands.Jitter.JitterIntake;
 import frc.robot.RobotContainer;
 import frc.robot.commands.Auto.AutoIndexAndSpindexCommand;
 import frc.robot.subsystems.IntakeSubsystem;
